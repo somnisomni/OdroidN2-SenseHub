d456208185cec053b3210f54393b7e905f1ace9f
somni <me@somni.one>
2023-07-23 17:54:13 +0900

    Add thermal zone monitor

---
+++ MQTT/publisher/modules/base/thermal_zone.py
@@ -0,0 +1,42 @@
+import const
+from modules.base.module import Module
+
+class ThermalZoneSysfileInfo:
+  def __init__(self, zone_num: int):
+    self.temp_sysfile = "/sys/class/thermal/thermal_zone{}/temp".format(zone_num)
+    self.type_sysfile = "/sys/class/thermal/thermal_zone{}/type".format(zone_num)
+
+class ThermalZone:
+  __discovery_config_common = {
+    "device_class": "temperature",
+    "unit_of_measurement": "°C",
+    "entity_category": "diagnostic",
+    "state_class": "measurement",
+    "expire_after": 20
+  }
+
+  def __init__(self, caller_module: Module, zone_num: int, zone_name: str = None):
+    self.ZONE_NUM = zone_num
+    self.SYSFILE_INFO = ThermalZoneSysfileInfo(zone_num)
+
+    self.DISCOVERY_TOPIC = caller_module.homeassistant_discovery_topic.replace(caller_module.homeassistant_unique_id, "thermalzone{}".format(zone_num))
+    self.DISCOVERY_CONFIG = dict(self.__discovery_config_common)
+    self.DISCOVERY_CONFIG.update({
+        "name": "{} {}".format(const.DEVICE_NAME, zone_name if zone_name else "Thermal Zone #{}".format(zone_num)),
+        "unique_id": caller_module.homeassistant_real_unique_id + "_thermalzone{}".format(zone_num),
+        "state_topic": caller_module.get_real_topic("thermalzone/state"),
+        "value_template": "{{ " + "value_json.thermalzone{}".format(zone_num) + " }}",
+    })
+
+  def get_values(self) -> dict:
+    key = "thermalzone{}".format(self.ZONE_NUM)
+    output = {}
+    output[key] = {}
+
+    try:
+      with open(self.SYSFILE_INFO.temp_sysfile, "r") as tempCall:
+        output[key] = int(tempCall.read().strip()) / 1000
+    except:
+      print("Failed to get temperature for thermal zone #{}!".format(self.ZONE_NUM))
+
+    return output
--- MQTT/publisher/modules/system_n2.py
+++ MQTT/publisher/modules/system_n2.py
@@ -2,6 +2,7 @@ from json import JSONEncoder
 from time import sleep
 from modules.base.module import Module
 from modules.base.cpu_cluster import CPUCluster
+from modules.base.thermal_zone import ThermalZone
 
 class System_N2(Module):
   def __init__(self):
@@ -10,24 +11,31 @@ class System_N2(Module):
     self.homeassistant_discovery_type = "sensor"
     self.homeassistant_unique_id = "system"
 
-    self.clusters = [CPUCluster(self, 0), CPUCluster(self, 2)]
+    self.cpu_clusters = [CPUCluster(self, 0), CPUCluster(self, 2)]
+    self.thermal_zones = [ThermalZone(self, 0, "CPU Temperature"), ThermalZone(self, 1, "DDR RAM Temperature")]
 
   @property
   def discovery_config(self) -> object:
     config = []
 
-    for cluster in self.clusters:
+    for cluster in self.cpu_clusters:
       config.append(cluster.DISCOVERY_CONFIG_CUR)
       config.append(cluster.DISCOVERY_CONFIG_MAX)
       config.append(cluster.DISCOVERY_CONFIG_MIN)
 
+    for zone in self.thermal_zones:
+      config.append(zone.DISCOVERY_CONFIG)
+
     return config
 
   def send_discovery(self) -> None:
-    for cluster in self.clusters:
+    for cluster in self.cpu_clusters:
       self.client.publish(cluster.DISCOVERY_TOPIC_CUR, JSONEncoder().encode(cluster.DISCOVERY_CONFIG_CUR), retain=True)
       self.client.publish(cluster.DISCOVERY_TOPIC_MAX, JSONEncoder().encode(cluster.DISCOVERY_CONFIG_MAX), retain=True)
       self.client.publish(cluster.DISCOVERY_TOPIC_MIN, JSONEncoder().encode(cluster.DISCOVERY_CONFIG_MIN), retain=True)
+
+    for zone in self.thermal_zones:
+      self.client.publish(zone.DISCOVERY_TOPIC, JSONEncoder().encode(zone.DISCOVERY_CONFIG), retain=True)
 
   def run(self) -> None:
     super().run()
@@ -37,8 +45,12 @@ class System_N2(Module):
         output = {}
 
         ## Get frequencies per cluster ##
-        for cluster in self.clusters:
+        for cluster in self.cpu_clusters:
           output.update(cluster.get_values())
+
+        ## Get thermal temperature per zone ##
+        for zone in self.thermal_zones:
+          output.update(zone.get_values())
 
         ## MQTT Publish ##
         self.client.publish(self.get_real_topic("system/state"), JSONEncoder().encode(output))
